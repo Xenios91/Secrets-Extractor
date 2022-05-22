d571c6bc7f4aaa1a869097a4b6b42cacae93757e
Corey <xenios91@protonmail.com>
2022-05-22 18:40:31 +0000

    added base64 extraction

--- packetUtil/passwordExtractor.go
+++ packetUtil/passwordExtractor.go
@@ -1,7 +1,8 @@
 package packetutil
 
 import (
-	"fmt"
+	"encoding/base64"
+	"log"
 	"strings"
 )
 
@@ -10,9 +11,25 @@ func (packetDetails *PacketDetails) FindPasswords() []string {
 	potentialPasswords := make([]string, 0)
 
 	headers := packetDetails.extractHTTPHeaders()
-	fmt.Println(headers)
-	kvp := packetDetails.extractKVP()
+	for i := 0; i < len(headers); i++ {
+		value := headers[i]
+		if strings.Contains(strings.ToLower(value), "authorization: basic") {
+			basicAuth := strings.Split(value, "authorization: basic")
+			if len(basicAuth > 0) {
+				decodedBase64, err := base64.StdEncoding.DecodeString(basicAuth[1])
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				decodedBase64String := string(decodedBase64)
+				if strings.Contains(decodedBase64String, ":") {
+					potentialPasswords = append(potentialPasswords, strings.Split(decodedBase64String, ":")[1])
+				}
+			}
+		}
+	}
 
+	kvp := packetDetails.extractKVP()
 	for i := 0; i < len(kvp); i++ {
 		value := kvp[i]
 		var password string
