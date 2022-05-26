8a5554489fc28b4c9724e5e728506c631216bf28
Corey <xenios91@protonmail.com>
2022-05-26 04:50:12 +0000

    added live packet capture

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	packetUtils "passession-extractor/packetUtil"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
@@ -13,36 +19,126 @@ import (
 
 var secretsArray []packetUtils.Secrets = make([]packetUtils.Secrets, 0)
 
+func getDeviceList() []pcap.Interface {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Devices Found:")
+	for deviceNum, device := range devices {
+		fmt.Printf("\n%d. %s\nDescription: %s\n", deviceNum, device.Name, device.Description)
+
+		var addresses strings.Builder
+		addresses.WriteString("Addresses: [")
+		addressCount := len(device.Addresses)
+		if addressCount == 0 {
+			addresses.WriteString("None")
+		}
+
+		for addressNum, address := range device.Addresses {
+			addresses.WriteString(address.IP.String())
+			if addressNum != addressCount-1 {
+				addresses.WriteString(", ")
+			}
+		}
+		addresses.WriteString("]")
+		fmt.Println(addresses.String())
+	}
+
+	return devices
+}
+
+func getDeviceSelection() pcap.Interface {
+	var input string
+	var shouldContinue string
+	var device pcap.Interface
+	devices := getDeviceList()
+
+	for shouldContinue != "y" {
+		fmt.Println("\nPlease select the device number you wish to capture on:")
+		fmt.Scanln(&input)
+
+		inputToInt, err := strconv.Atoi(input)
+		if err != nil {
+			continue
+		}
+
+		device = devices[inputToInt]
+
+		validContinue := false
+		for !validContinue {
+			fmt.Printf("You selected device: [%s], would you like to continue? y/n\n", device.Name)
+			fmt.Scanln(&shouldContinue)
+			if strings.ToLower(shouldContinue) == "y" || strings.ToLower(shouldContinue) == "n" {
+				validContinue = true
+			}
+		}
+
+	}
+
+	return device
+}
+
+func getLiveCaptureHandle() *pcap.Handle {
+	device := getDeviceSelection()
+	var snapshot_len int32 = 1024
+	var promiscuous bool = true
+	var timeout time.Duration = 30 * time.Second
+
+	handle, err := pcap.OpenLive(device.Name, snapshot_len, promiscuous, timeout)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return handle
+}
+
 func main() {
 	var pcapFile string
 	var outputFile string
-	var moreDetails bool
+	var handle *pcap.Handle
 
 	flag.StringVar(&pcapFile, "file", "", "-file=myCapture.pcap")
 	flag.StringVar(&outputFile, "output", "secrets_dump.json", "-output=secrets.json")
-	flag.BoolVar(&moreDetails, "details", false, "-details=false")
 	flag.Parse()
 
-	if moreDetails {
-		log.Println("I'm not very good with names, go away...\nanywho, contact me if you have additional questions, hope you enjoy.")
-	}
-
-	if _, err := os.Stat(pcapFile); errors.Is(err, os.ErrNotExist) {
-		flag.PrintDefaults()
-		log.Fatalf("file [%s] not found!\n", pcapFile)
-	}
-
-	handle, err := pcap.OpenOffline(pcapFile)
-	if err != nil {
-		log.Fatal(err)
+	if len(pcapFile) == 0 {
+		handle = getLiveCaptureHandle()
+		log.Println("Starting packet capture... Use CTRL + C to end capture")
+	} else {
+		if _, err := os.Stat(pcapFile); errors.Is(err, os.ErrNotExist) {
+			flag.PrintDefaults()
+			log.Fatalf("file [%s] not found!\n", pcapFile)
+		}
+		var err error
+		handle, err = pcap.OpenOffline(pcapFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Checking packets from file: [%s]\n", pcapFile)
 	}
 	defer handle.Close()
 
-	log.Printf("Checking packets from file: [%s]\n", pcapFile)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	terminateCapture := false
+
+	//go routine to check for os signals to gracefully end capture
+	go func() {
+		<-sigs
+		fmt.Println("Ending capture... please wait...")
+		terminateCapture = true
+	}()
+
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	counter := 1
 
 	for packet := range packetSource.Packets() {
+		if terminateCapture {
+			break
+		}
+
 		packetDetails := packetUtils.NewPacketDetails(&packet)
 		if packetDetails.GetApplicationLayer() == nil {
 			continue //no application layer, we skip this packet
